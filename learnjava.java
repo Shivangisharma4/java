49e32fe70b0c4575e5e8a3e91d0f870a60dc34be
shivangisharma4 <shivangi.sharma7004@gmail.com>
2025-06-21 22:04:41 +0530

    java code

--- learnjava.java
+++ learnjava.java
@@ -1,5 +1,37 @@
 public class learnjava {
     public static void main(String[] args) {
-        System.out.println("hhel");
+        // the followinf line prints hello
+
+        /*
+        *HI
+        HI
+        */
+
+       //-129 to 127
+// 8 bits in the memory
+//1byte = 8 bits
+//        byte num=100; {
+// System.out.println(num);
+        // short num = 100;{
+        // System.out.println(num);
+
+    //     int num = 100;{ // for 32 bits (large range nums)
+    //     System.out.println(num);
+    //    }
+
+    // long num = 100;{ // for 64 bits
+    //     System.out.println(num);
+    //    }
+
+    // double num = 100;{ // for 64 bits w decimal
+    //     System.out.println(num);
+    //    }
+
+    // float num = 100;{ // for 32 bits w decimal
+    //     System.out.println(num);
+    //    }
+
+    
+
     }
 }
